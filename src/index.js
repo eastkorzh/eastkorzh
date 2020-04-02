dbb2a6fe66a1f3e239de3dce61e4f34a0bb2fbac
= <=>
2020-04-02 12:38:40 +0300

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './store';
+import storeAPI from './store';
 
 import App from './App';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
+	<Provider store={storeAPI.store}>
+		<PersistGate loading={null} persistor={storeAPI.persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,17 +1,36 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 
 import rootReducer from './rootReducer';
 import rootSagas from './rootSagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: hardSet,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = composeEnhancers(
+  applyMiddleware(sagaMiddleware)
 );
+
+const store = createStore(
+  persistedReducer,
+  middleware,
+);
+
+const persistor = persistStore(store);
 
 rootSagas.forEach(saga => sagaMiddleware.run(saga));
 
-export default store;
+export default { store, persistor };
+
